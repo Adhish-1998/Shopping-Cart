71076f4de8cfd3ea220a4c20a3a322ece3aebe41
Adhish Basak <adhishbasak98@gmail.com>
2022-07-25 12:11:06 +0530

    User and Product Schema Added

---
+++ src/models/productModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    currencyId: {
+        type: String,
+        required: true
+    },
+    currencyFormat: {
+        type: String,
+        required: true
+    },
+    isFreeShipping: {
+        type: Boolean,
+        default: false
+    },
+    productImage: {
+        type: String,
+        required: true
+    }, 
+    style: { 
+    type: String, 
+    trim: true 
+    },
+    availableSizes: {
+        type: [String],
+        required: true,
+        enum: ["S", "XS","M","X", "L","XXL", "XL"],
+        trim: true
+    },
+    installments: { 
+        type: Number 
+    },
+    deletedAt: {
+        type: Date
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('product', productSchema);
